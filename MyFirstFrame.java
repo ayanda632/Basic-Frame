9a6916fbee40ab9a2e445ac4518bc0008046cbb3
ayanda632 <amakhanya808@icloud.com>
2025-11-23 02:48:39 +0200

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author amakh
+ */
+public class MyFirstFrame extends JFrame {
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(200, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+}
